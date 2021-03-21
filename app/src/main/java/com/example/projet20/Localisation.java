fef42f2784cb2ae3edd2615c2e294186dd9a8ba5
Nadine Ouaziz <71225818+nadineOzz@users.noreply.github.com>
2021-03-21 20:19:31 +0100

    Ceci est un test

--- app/src/main/java/com/example/projet20/Localisation.java
+++ app/src/main/java/com/example/projet20/Localisation.java
@@ -36,6 +36,7 @@ import java.io.IOException;
 import java.util.List;
 import java.util.Locale;
 //import fr.android.mapsi.R;
+//test
 
 public class Localisation extends FragmentActivity implements OnMapReadyCallback, LocationListener {
     private GoogleMap mMap;
